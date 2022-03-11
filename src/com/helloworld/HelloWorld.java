51f5db5b350b6ba7ba34b5641410507a55ab875b
Gustavo Amaral <l.do.amaral.correa@avanade.com>
2022-03-11 11:14:18 -0300

    Upate code

--- src/com/helloworld/HelloWorld.java
+++ src/com/helloworld/HelloWorld.java
@@ -4,7 +4,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Client cl = new Client();
+		
+		cl.setId(1L);
+		cl.setDescricao("Ola mundo");
+		
+		System.out.println(cl.getId());
+		System.out.println(cl.getDescricao());
+		
 	}
 
 }
